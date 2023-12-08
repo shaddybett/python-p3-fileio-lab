c1b11e3963faaebafd300392122a1aed6858334f
Shaddy <shadybett540@gmail.com>
2023-12-08 09:40:23 +0300

    test complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    file_path = file_name.with_suffix(".txt")
+    with open(file_path, "w") as file:
+        file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_to_file(file_name, append_content):
+    file_path = file_name.with_suffix(".txt")
+    with open(file_path, "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_path = file_name.with_suffix(".txt")
+    with open(file_path, "r") as file:
+        content = file.read()
+    return content
--- lib/testing/file_io_test.py
+++ lib/testing/file_io_test.py
@@ -1,4 +1,4 @@
-from file_io import write_file, append_file, read_file
+from file_io import write_file, append_to_file, read_file
 
 def test_write_file(tmp_path):
     """Test write_file()"""
@@ -16,7 +16,7 @@ def test_append_file(tmp_path):
     file_content = "This is a test content."
     append_content = "\nAppended content."
     write_file(file_name, file_content)
-    append_file(file_name, append_content)
+    append_to_file(file_name, append_content)
     with open(f'{file_name}.txt', 'r') as f:
         file_content_read = f.read()
     assert file_content_read == file_content + append_content
